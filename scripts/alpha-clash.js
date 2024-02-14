39ef1754e26ac6bdc6b4a7b3749d3354fedb9341
rahidhaque <ontirahid@yahoo.com>
2024-02-14 15:22:19 +0600

    Play now to move to playground done

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,7 @@
+function play(){
+    const homeSection= document.getElementById('home');
+    homeSection.classList.add('hidden');
+
+    const playGroundSection= document.getElementById('play-ground');
+    playGroundSection.classList.remove('hidden');
+}
